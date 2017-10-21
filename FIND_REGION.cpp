a32282cfa17e78da12b4b233c5894c7d4508b8e8
ahojaij <ahojaij@uwaterloo.ca>
2017-10-21 17:20:25 -0400

    code clean up. Added a threshold to get similar colors that are off by the threshold

--- FIND_REGION.cpp
+++ FIND_REGION.cpp
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <cmath>
 
 using namespace cv;
 
 void show_mat(const cv::Mat &image, std::string const &win_name);
 
-cv::Mat& flood_fill_mat(cv::Mat &mat, int xPixel, int yPixel, uchar *targetColor, uchar *replacementColor);
-
-cv::Mat &invert_mat(cv::Mat &mat);
-
-cv::Mat& invert_mat_pointer(cv::Mat &mat);
+cv::Mat& flood_fill_mat(cv::Mat &mat, int xPixel, int yPixel, uchar *targetColor, uchar *replacementColor, int colorThreshold);
 
 int main(int argc, char **argv) {
     if (argc != 6) {
@@ -28,9 +25,17 @@ int main(int argc, char **argv) {
 
     show_mat(image, "Input");
 
-    uchar targetColor [] = {255, 255, 255};    
+	    Vec3b *node;
+	    node = image.ptr<Vec3b>(atoi(argv[2]));
+    printf("node[xPixel][yPixel]: %u, %u, %u\n", node[atoi(argv[3])][0], node[atoi(argv[3])][1], node[atoi(argv[3])][2]);
+    uchar targetColor [3]; //  = {255, 255, 255};
+    // targetColor is the color of pixel at target location
+    targetColor[0] = node[atoi(argv[3])][0];
+    targetColor[1] = node[atoi(argv[3])][1];
+    targetColor[2] = node[atoi(argv[3])][2];   
     uchar replacementColor [] = {0, 0, 0};
-    image = flood_fill_mat(image, atoi(argv[2]), atoi(argv[3]), targetColor, replacementColor);
+//    int colorThreshold = atoi(argv[4]);
+    image = flood_fill_mat(image, atoi(argv[2]), atoi(argv[3]), targetColor, replacementColor, atoi(argv[4]));
     show_mat(image, "Output");
 
     imwrite(argv[5], image);
@@ -38,7 +43,7 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-cv::Mat& flood_fill_mat(cv::Mat &mat, int xPixel, int yPixel, uchar *targetColor, uchar *replacementColor) {    
+cv::Mat& flood_fill_mat(cv::Mat &mat, int xPixel, int yPixel, uchar *targetColor, uchar *replacementColor, int colorThreshold) {    
     int channels = mat.channels();
 
     int nRows = mat.rows;
@@ -47,82 +52,26 @@ cv::Mat& flood_fill_mat(cv::Mat &mat, int xPixel, int yPixel, uchar *targetColor
     int i, j;
     switch (channels) {
         case 3: {
-//            RGB image
-	    //printf("step 1: xPixel= %i, yPixel= %i\n", xPixel, yPixel);
-	    //std::cin>> channels;
+//            RGB image // It is assumed that we only get RGB images
 	    Vec3b *node;
 	    node = mat.ptr<Vec3b>(xPixel);
-	    //Vec3b *node;
-	    //node = &mat.at<Vec3b>(xPixel, yPixel);
-	    //printf("step 2\n");
 	    if (replacementColor[0] == targetColor[0] && replacementColor[1] == targetColor[1] && replacementColor[2] == targetColor[2])
 		return mat;
-	    if (node[yPixel][0] != targetColor[0] || node[yPixel][1] != targetColor[1] || node[yPixel][2] != targetColor[2])
+	    if (std::abs(node[yPixel][0] - targetColor[0]) > colorThreshold || std::abs(node[yPixel][1] - targetColor[1]) > colorThreshold || std::abs(node[yPixel][2] - targetColor[2]) > colorThreshold)
 		return mat;
-	    //printf("step 3: node[0]: %u, targetColor[0]: %u \n", node[yPixel][0], targetColor[0]);
-	    if (node[yPixel][0] != 255)
-		printf("step 3: node[0]: %u, targetColor[0]: %u \n", node[yPixel][0], targetColor[0]);
 	    node[yPixel][0] = replacementColor[0];
 	    node[yPixel][1] = replacementColor[1];
 	    node[yPixel][2] = replacementColor[2];
-	    //printf("step 4\n");
 	    if (yPixel > 0)
-	    	mat = flood_fill_mat(mat, xPixel, yPixel - 1, targetColor, replacementColor);	    
+	    	mat = flood_fill_mat(mat, xPixel, yPixel - 1, targetColor, replacementColor, colorThreshold);	    
 	    if (yPixel < nCols - 1)
-	    	mat = flood_fill_mat(mat, xPixel, yPixel + 1, targetColor, replacementColor);
+	    	mat = flood_fill_mat(mat, xPixel, yPixel + 1, targetColor, replacementColor, colorThreshold);
 	    if (xPixel > 0)
-	    	mat = flood_fill_mat(mat, xPixel - 1, yPixel, targetColor, replacementColor);
+	    	mat = flood_fill_mat(mat, xPixel - 1, yPixel, targetColor, replacementColor, colorThreshold);
 	    if (xPixel < nRows - 1)
-	    	mat = flood_fill_mat(mat, xPixel + 1, yPixel, targetColor, replacementColor);
-
-	    //printf("step 5\n");
-//            Vec3b *p;
-//           for (i = 0; i < nRows; ++i) {
-//                p = mat.ptr<Vec3b>(i);
-//                for (j = 0; j < nCols; ++j) {
-//                    p[j][0] = ~p[j][0];
-//                    p[j][1] = ~p[j][1];
-//                    p[j][2] = ~p[j][2];
-//                }
-//            }
+	    	mat = flood_fill_mat(mat, xPixel + 1, yPixel, targetColor, replacementColor, colorThreshold);
         }
     }
-    return mat;
-}
-
-
-cv::Mat& invert_mat_pointer(cv::Mat &mat) {
-    int channels = mat.channels();
-
-    int nRows = mat.rows;
-    int nCols = mat.cols;
-
-    int i, j;
-    switch (channels) {
-        case 1: {
-//            gray scale image
-            uchar *p;
-            for (i = 0; i < nRows; ++i) {
-                p = mat.ptr<uchar>(i);
-                for (j = 0; j < nCols; ++j) {
-                    p[j] = ~p[j];
-                }
-            }
-        }
-        case 3: {
-//            RGB image
-            Vec3b *p;
-            for (i = 0; i < nRows; ++i) {
-                p = mat.ptr<Vec3b>(i);
-                for (j = 0; j < nCols; ++j) {
-                    p[j][0] = ~p[j][0];
-                    p[j][1] = ~p[j][1];
-                    p[j][2] = ~p[j][2];
-                }
-            }
-        }
-    }
-
     return mat;
 }
 
